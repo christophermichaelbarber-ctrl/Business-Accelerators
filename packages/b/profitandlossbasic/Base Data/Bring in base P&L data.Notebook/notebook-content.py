632d7a7a03658bba20be14b6e07fb98d92fd2b11
christophermichaelbarber-ctrl <christophermichaelbarber@hotmail.com>
2025-10-07 09:54:18 +0100

    Committing 2 items from workspace f4f0fd69-066c-47fa-9b3e-b287bdf1dde2

---
+++ packages/b/profitandlossbasic/Base Data/Bring in base P&L data.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "3c875536-2d75-46e8-9e89-41498b7103d9",
+# META       "default_lakehouse_name": "profitandlossbasic_lakehouse",
+# META       "default_lakehouse_workspace_id": "f4f0fd69-066c-47fa-9b3e-b287bdf1dde2",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "3c875536-2d75-46e8-9e89-41498b7103d9"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
