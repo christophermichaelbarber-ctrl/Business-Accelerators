b74c6563e04fd0f81763aa4be542ccf50333078a
christophermichaelbarber-ctrl <christophermichaelbarber@hotmail.com>
2025-10-07 11:00:17 +0100

    Committing 4 items from workspace f4f0fd69-066c-47fa-9b3e-b287bdf1dde2

---
+++ packages/b/profitandlossbasic/Lakehouse/testing ingestion.Notebook/notebook-content.py
@@ -0,0 +1,38 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+import pandas as pd
+
+wrangler_sample_df = pd.read_csv("https://aka.ms/wrangler/titanic.csv")
+display(wrangler_sample_df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark",
+# META   "editable": true
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
