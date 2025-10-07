c5def0233500f89fd33099ec9cd3de4332c97bfe
christophermichaelbarber-ctrl <christophermichaelbarber@hotmail.com>
2025-10-07 10:38:30 +0100

    Committing 1 item from workspace f4f0fd69-066c-47fa-9b3e-b287bdf1dde2

--- packages/b/profitandlossbasic/Lakehouse/Bring in base P&L data.Notebook/notebook-content.py
+++ packages/b/profitandlossbasic/Lakehouse/Bring in base P&L data.Notebook/notebook-content.py
@@ -22,13 +22,18 @@
 
 # CELL ********************
 
-# Welcome to your new notebook
-# Type here in the cell editor to add code!
+import pandas as pd
+
+wrangler_sample_df = pd.read_csv("https://aka.ms/wrangler/titanic.csv")
+display(wrangler_sample_df)
+
+
 
 
 # METADATA ********************
 
 # META {
 # META   "language": "python",
-# META   "language_group": "synapse_pyspark"
+# META   "language_group": "synapse_pyspark",
+# META   "editable": true
 # META }
