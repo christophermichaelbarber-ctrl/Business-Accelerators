e00c376adfd0c00ba2897223be5d2993ab09ddce
christophermichaelbarber-ctrl <christophermichaelbarber@hotmail.com>
2025-10-07 12:33:13 +0100

    Committing 2 items from workspace f4f0fd69-066c-47fa-9b3e-b287bdf1dde2

--- packages/b/profitandlossadvanced/Lakehouse/IngestPLdata/notebook-content.py
+++ packages/b/profitandlossadvanced/Lakehouse/IngestPLdata/notebook-content.py
@@ -9,7 +9,7 @@
 # META   "dependencies": {
 # META     "lakehouse": {
 # META       "default_lakehouse": "3c875536-2d75-46e8-9e89-41498b7103d9",
-# META       "default_lakehouse_name": "profitandlossbasic_lakehouse",
+# META       "default_lakehouse_name": "profitandlossadvanced_lakehouse",
 # META       "default_lakehouse_workspace_id": "f4f0fd69-066c-47fa-9b3e-b287bdf1dde2",
 # META       "known_lakehouses": [
 # META         {
@@ -37,7 +37,7 @@ display(accounts_df)
 
 # CELL ********************
 
-accounts_df.to_parquet("abfss://GitIntegrationFabricTrial@onelake.dfs.fabric.microsoft.com/profitandlossbasic_lakehouse.Lakehouse/Files/accounts.parquet")
+accounts_df.to_parquet("abfss://GitIntegrationFabricTrial@onelake.dfs.fabric.microsoft.com/profitandlossadvanced_lakehouse.Lakehouse/Files/accounts.parquet")
 
 # METADATA ********************
 
@@ -48,18 +48,19 @@ accounts_df.to_parquet("abfss://GitIntegrationFabricTrial@onelake.dfs.fabric.mic
 
 # CELL ********************
 
-accounts_df_table = spark.read.parquet("abfss://GitIntegrationFabricTrial@onelake.dfs.fabric.microsoft.com/profitandlossbasic_lakehouse.Lakehouse/Files/accounts.parquet")
+accounts_df_table = spark.read.parquet("abfss://GitIntegrationFabricTrial@onelake.dfs.fabric.microsoft.com/profitandlossadvanced_lakehouse.Lakehouse/Files/accounts.parquet")
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
 accounts_df_table.write.mode("overwrite").format("delta").saveAsTable("Accounts_table")
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
 
 # METADATA ********************
 
